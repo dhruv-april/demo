8661a9e98f69aaf60a1e6e9aecffae0c8f4ad01f
Dhruv Solanki <dhruv@aprilinnovations.com>
2025-03-18 16:46:59 +0530

    README added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Demo App
+It is a backend service that exposes a single REST API to create/update/delete records. 
+
+# Requirement
+Node version: v20.15.0
+yarn version: v1.22.22
+mongodb
+
+# How to run
+yarn install
+yarn start
+
+<!-- dev -->
+yarn dev (for development purpose using nodemon)
--- db/db_connection.js
+++ db/db_connection.js
@@ -1,8 +1,12 @@
 const { default: mongoose } = require("mongoose");
 
+// can be setup to take from env file
+const DB_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'demo';
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/demo', {
+    await mongoose.connect(`${DB_URL}/${DB_NAME}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
